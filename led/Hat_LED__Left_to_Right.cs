978ad04f6cb3cd9393007feec75f511db738c2ec
Omar Shishani <omarshishanischool@gmail.com>
2022-09-24 00:23:52 -0400

    Fix speed up slow down function

--- led/Hat_LED__Left_to_Right.cs
+++ led/Hat_LED__Left_to_Right.cs
@@ -5,18 +5,44 @@ void setup()
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
+
 }
+
+int delayTime; 
 
 void loop()
 {
   // Do a for loop that counts up to 10 
   // and turns on the LED each time
   
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 20; i++)
   {
     // create a variable i multiplied by 100
     // and assign it to the variable delayTime
-    int delayTime = i * 100;
+    int delayMultiplier = 50; 
+    if (i < 10){
+      delayTime = i * delayMultiplier;
+    }
+    // When i is over 10, make lights start slowing down again.
+    else {
+      // int delayTime = ((i - 2) * i ) * 100;
+      delayTime = ( i + ( -2 * (i - 10) ) ) * delayMultiplier;
+    }
+
+
+    // 10 * 100 = 1000
+    // (11 + x) * 100 = 900
+    // (11 + x) * 1 = 9
+    // (11 + x) = 9
+    // (x) = -2
+
+    // (12 + x) * 100 = 800
+    // (12 + x) = 8
+    // (x) = -4
+
+    // (13 + x) * 100 = 700
+    // (13 + x) = 7
+    // (x) = -6
 
     digitalWrite(12, HIGH);  
     digitalWrite(11, LOW);  
