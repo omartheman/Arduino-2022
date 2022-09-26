60498215060b0839ce67339ae6178e7063c49d44
Omar Shishani <omarshishanischool@gmail.com>
2022-09-26 14:07:40 -0400

    Make sound sensor stay on after yelling

--- sound-sensor/sound-sendor.cs
+++ sound-sensor/sound-sendor.cs
@@ -5,16 +5,23 @@ void setup() {
   
 }
 
+int counter = 0; 
+
 void loop() {
   Serial.println(analogRead(A0));
   delay(100);
   
   
   if (analogRead(A0) > 100){
+    counter++; 
+  }
+
+  if (counter % 2 == 0){
     digitalWrite(7, HIGH);  
   }
   else {
-    digitalWrite(7, LOW);  
+    digitalWrite(7, LOW); 
   }
+
 
 } 
