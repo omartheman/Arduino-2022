f66a5481b61bb193a3f1e4eaa0f07356b4673834
Omar Shishani <omarshishanischool@gmail.com>
2025-11-28 17:56:50 -0800

    Make smile and eyes blink

--- Smiley Face LED/smiley-face-led.ino
+++ Smiley Face LED/smiley-face-led.ino
@@ -1,3 +1,5 @@
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -7,15 +9,51 @@ void setup() {
   pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Eyes
+  digitalWrite(12, LOW);
+  delay(200); 
   digitalWrite(12, HIGH);
+  digitalWrite(11, LOW); 
+  delay(200); 
   digitalWrite(11, HIGH);
+  delay(200); 
+
+  //Smiley 
   digitalWrite(10, HIGH);
   digitalWrite(9, HIGH);
   digitalWrite(8, HIGH);
   digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, HIGH);
+
+  digitalWrite(10, LOW);
+  delay(200);
+  digitalWrite(10, HIGH);
+  digitalWrite(8, LOW);
+  delay(200);
+  digitalWrite(8, HIGH);
+  digitalWrite(5, LOW);
+  delay(200);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, LOW);
+  delay(200);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, LOW);
+  delay(200);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  delay(200);
+  digitalWrite(7, HIGH);
+  digitalWrite(9, LOW);
+  delay(200);
+  digitalWrite(9, HIGH);
 }
