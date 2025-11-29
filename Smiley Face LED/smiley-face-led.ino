131bd7cd9ba46305a8fb61cc89b8de12df94fa4e
Omar Shishani <omarshishanischool@gmail.com>
2025-11-28 17:56:36 -0800

    Add smiley face code

---
+++ Smiley Face LED/smiley-face-led.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+}
